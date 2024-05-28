9af4024ff6a4ddae9cccb34901155775b0b267af
Junpyo Won <thewon4155@naver.com>
2024-05-29 07:41:43 +0900

    Add files via upload

---
+++ backtesting.py
@@ -0,0 +1,42 @@
+import pyupbit
+import pandas as pd
+import numpy as np
+import time
+
+# drr = Daily Rate Of Return
+# crr = Cumulative Rate Of Return
+# mdd = Max Draw Down, dd = Draw Down
+
+coin = "KRW-BTC"
+interval = "day"
+fees = 0.0005
+day_count = 1300
+K = 1
+
+date = None
+dfs = [ ]
+
+for i in range(day_count // 200 + 1):
+    if i < day_count // 200 :
+        df = pyupbit.get_ohlcv(coin, to = date, interval = interval)
+        date = df.index[0]
+    elif day_count % 200 != 0 :
+        df = pyupbit.get_ohlcv(coin, to = date, interval = interval, count = day_count % 200)
+    else :
+        break
+    dfs.append(df)
+    time.sleep(0.1)
+
+df = pd.concat(dfs).sort_index()
+
+df['range'] = df['high'].shift(1) - df['low'].shift(1)
+df['targetPrice'] = df['open'] + df['range'] * K
+df['drr'] = np.where(df['high'] > df['targetPrice'], (df['close'] / (1 + fees)) / (df['targetPrice'] * (1 + fees)) - 1 , 0)
+
+df['crr'] = (df['drr'] + 1).cumprod() - 1
+df['dd'] = -(((df['crr'] + 1).cummax() - (df['crr'] + 1)) / (df['crr'] + 1).cummax())
+
+print("기간수익률 :", df['crr'][-1] * 100, "% , 최대손실률 :", df['dd'].min() * 100, "% , 수수료 :", fees * 100, "%")
+print("알고리즘 적용 없을 시 수익률 :", ((df['close'][-1]/(1+fees))/(df['open'][0]*(1+fees))-1) * 100,"%")
+
+df.to_excel("crypto_history_k_1.xlsx")
---
+++ coin_autotrade_ver1.py
@@ -0,0 +1,60 @@
+import pyupbit
+import datetime
+import time, calendar
+import auth
+
+def get_targetPrice(df, K) :
+    range = df['high'][-2] - df['low'][-2]
+    return df['open'][-1] + range * K
+
+def buy_all(coin) :
+    balance = upbit.get_balance("KRW") * 0.9995
+    if balance >= 5000 :
+        print(upbit.buy_market_order(coin, balance))
+
+def sell_all(coin) :
+    balance = upbit.get_balance(coin)
+    price = pyupbit.get_current_price(coin)
+    if price * balance >= 5000 :
+        print(upbit.sell_market_order(coin, balance))
+
+
+if __name__ == '__main__': 
+    try:
+        upbit = pyupbit.Upbit(auth.access, auth.secret)
+
+        # set variables
+        coin = "KRW-BTC"
+        fees = 0.0005
+        K = 0.5
+    
+        start_balance = upbit.get_balance("KRW")
+        df = pyupbit.get_ohlcv(coin, count = 2, interval = "day")
+        targetPrice = get_targetPrice(df, K)
+        print(datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S'), "\t\tBalance :", start_balance, "KRW \t\tYield :", ((start_balance / start_balance) - 1) * 100, "% \t\tNew targetPrice :", targetPrice, "KRW")
+
+        while True :
+            now = datetime.datetime.now()
+            if now.hour == 9 and now.minute == 0 and now.second == 0:
+                sell_all(coin)
+                df = pyupbit.get_ohlcv(coin, count = 2, interval = "day")
+                targetPrice = get_targetPrice(df, K)
+                time.sleep(1)
+
+                cur_balance = upbit.get_balance("KRW")
+                print(now.strftime('%y/%m/%d %H:%M:%S'), "\t\tBalance :", cur_balance, "KRW \t\tYield :", ((cur_balance / start_balance) - 1) * 100, "% \t\tNew targetPrice :", targetPrice, "KRW")
+            
+            elif targetPrice <= pyupbit.get_current_price(coin) :
+                buy_all(coin)
+                
+                start_time = df.index[-1]
+                end_time = start_time + datetime.timedelta(days=1)
+                if end_time > now :
+                    print((end_time - now).seconds)
+                    time.sleep((end_time - now).seconds - 60)
+    
+            time.sleep(1)
+
+    except Exception as e:
+        print(e)
+        time.sleep(1)
